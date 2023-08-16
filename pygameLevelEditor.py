c92cb462828f4c68347e3137e4f3d29fc2766b36
sahan <sahan.m.rajapaksha@gmail.com>
2023-08-17 02:00:58 +0530

    commit

--- pygameLevelEditor.py
+++ pygameLevelEditor.py
@@ -8,5 +8,16 @@ SCREEN_HEIGHT = 600
 LOWER_MARGIN = 100
 SIDE_MARGIN = 300
 
+run = True
 
+screen = pg.display.set_mode((SCREEN_WIDTH + SIDE_MARGIN , SCREEN_HEIGHT + LOWER_MARGIN))
+
+pg.display.set_caption('Level Editor')
+
+while run:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = False 
+ 
+pg.quit()
 
