d7e9f3b57af8b98054abf9a6b367c9ec37db07ae
sahan <sahan.m.rajapaksha@gmail.com>
2023-08-17 02:16:56 +0530

    background images

--- pygameLevelEditor.py
+++ pygameLevelEditor.py
@@ -14,10 +14,22 @@ screen = pg.display.set_mode((SCREEN_WIDTH + SIDE_MARGIN , SCREEN_HEIGHT + LOWER
 
 pg.display.set_caption('Level Editor')
 
+#Images
+bg1 = pg.image.load('Images/bg1.png').convert_alpha()
+bg2 = pg.image.load('Images/bg2.png').convert_alpha()
+
+def draw_Background():
+    screen.blit(bg1,(0.0))
+
 while run:
+
+    draw_Background()
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False 
  
+    pg.display.update()
+
 pg.quit()
 
